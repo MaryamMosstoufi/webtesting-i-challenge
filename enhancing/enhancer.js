4bb5c1cfe339077b825f7facaf8e3fb7512edf4c
MaryamMosstoufi <maryam@zooxoos.com>
2020-08-20 10:52:25 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -13,10 +13,10 @@ function success(item) {
 function fail(item) {
   if (item.enhancement < 15) {
     item.durability -= 5;
-  } else if (item.enhancement >= 15) {
+  } else if (item.enhancement >= 15 && item.enhancement < 16) {
     item.durability -= 10
   } else if (item.enhancement > 16) {
-    item.enhancement -= 1
+    item.durability -= 1
   }
   return { ...item };
 }
